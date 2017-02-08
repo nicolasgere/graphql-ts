0e2538f076e91244a7d43ce01466ee6cdb6932f5
Nicolas <nicolas@MacBook-Air-de-Marina.local>
2017-02-07 19:47:06 -0500

    fix test

--- exemple/test/field.ts
+++ exemple/test/field.ts
@@ -122,7 +122,7 @@ it('should have note', (done) => {
     {
       user(firstName:"drago"){
         notes
-        
+
       }
     }
   `).then((res: any) => {
@@ -140,7 +140,7 @@ it('should be a date using the scalar', (done) => {
       }
     }
   `).then((res: any) => {
-      expect(res.data.user.birthday).to.equal('1990-10-10T04:00:00.000Z');
+      expect(res.data.user.birthday).to.contain('1990-10-10');
       done();
     }).catch((err) => {
       done(err);
